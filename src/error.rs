e7fbb88e9262eb85620be605e4d590fc49c70fcb
Reidond <reidond@gmail.com>
2020-02-24 17:30:17 +0200

    feat: Add error impl

---
+++ src/error.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub enum ClevelError {
+  SIMPLE(String),
+}
+
+impl ClevelError {
+  pub fn simple<T: Into<String>>(msg: T) -> ClevelError {
+    ClevelError::SIMPLE(msg.into())
+  }
+}
+
+impl std::fmt::Display for ClevelError {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    match *self {
+      ClevelError::SIMPLE(ref s) => write!(f, "{}", s),
+    }
+  }
+}
+
+impl std::error::Error for ClevelError {
+  fn cause(&self) -> Option<&dyn std::error::Error> {
+    match *self {
+      ClevelError::SIMPLE(ref _s) => Some(self),
+    }
+  }
+}
+
+impl From<std::string::FromUtf8Error> for ClevelError {
+  fn from(_: std::string::FromUtf8Error) -> ClevelError {
+    ClevelError::SIMPLE("Unable to convert UTF-8 to string".to_string())
+  }
+}
