e3afa9fa25d9bab5e23dc40df31451f4bcd7ee8d
Reidond <reidond@gmail.com>
2020-02-27 19:26:54 +0200

    feat: Integrate VuetifyDialog

--- clevel_app/src/main.js
+++ clevel_app/src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue';
+import VuetifyDialog from 'vuetify-dialog';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify-dialog/dist/vuetify-dialog.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VuetifyDialog, {
+  context: {
+    vuetify,
+  },
+});
 
 new Vue({
   router,
