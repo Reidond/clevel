3f21158d2e35a9b8d6a345fbe125ff0c884175df
Reidond <reidond@gmail.com>
2020-02-27 19:28:11 +0200

    feat: Add RemoveModel component

---
+++ clevel_app/src/components/RemoveModel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-btn color="error" outlined @click="removeModel(modelId)">Remove</v-btn>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'RemoveModel',
+  props: {
+    modelId: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    ...mapActions(['removeModel']),
+  },
+};
+</script>
