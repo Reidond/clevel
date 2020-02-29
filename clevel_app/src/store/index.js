b96cd9e70c629eb27b9f9e297ec30ae67d7bdd18
Reidond <reidond@gmail.com>
2020-02-29 21:53:39 +0200

    feat: Add persistance to vuex

--- clevel_app/src/store/index.js
+++ clevel_app/src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersist from 'vuex-persist';
 import modelsModule from './modules/models';
+
+const vuexPersist = new VuexPersist({
+  key: 'clevel-app',
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   state: {},
   mutations: {},
   actions: {},
