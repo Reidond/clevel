fce36526500c3214157754fbaf3fc80c52fec75e
Reidond <reidond@gmail.com>
2020-02-29 21:52:36 +0200

    feat: Move app-bar and main container from App

--- clevel_app/src/App.vue
+++ clevel_app/src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar app dense class="mb-8">
-      <v-toolbar-title>Clevel</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <AddNew />
-
-      <FindHeadFromModels />
-    </v-app-bar>
-
-    <v-container fluid>
-      <router-view></router-view>
-    </v-container>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import AddNew from '@/components/AddNew.vue';
-import FindHeadFromModels from '@/components/FindHeadFromModels.vue';
-
 export default {
   name: 'App',
-  components: {
-    AddNew,
-    FindHeadFromModels,
-  },
+  components: {},
 };
 </script>
---
+++ clevel_app/src/views/ModelBoard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-app-bar app dense class="mb-8">
+      <v-toolbar-title>Clevel</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <AddNew />
+
+      <FindHeadFromModels />
+    </v-app-bar>
+
+    <v-container fluid class="mt-10">
+      <v-row v-if="isModelsEmpty" align-content="center" align="stretch">
+        <v-col v-for="model in models" :key="model.name" cols="6">
+          <Table :model="model" />
+        </v-col>
+      </v-row>
+      <v-row v-else align-content="center" align="stretch">
+        <v-col cols="6" class="text-center">
+          <p class="title">
+            Create some models using the app bar "Add New" button...
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import AddNew from '@/components/AddNew.vue';
+import FindHeadFromModels from '@/components/FindHeadFromModels.vue';
+import Table from '../components/Table.vue';
+
+export default {
+  components: {
+    Table,
+    AddNew,
+    FindHeadFromModels,
+  },
+  data: () => ({}),
+  computed: {
+    ...mapState({
+      models: state => state.modelsModule.models.data,
+    }),
+    isModelsEmpty() {
+      return this.models.length > 0;
+    },
+  },
+  methods: {
+    ...mapActions(['setCurrentModel']),
+  },
+};
+</script>
