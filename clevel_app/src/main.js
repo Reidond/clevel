fdd89c655a11e0f4d907aebdaef5209b03e2be0b
Reidond <reidond@gmail.com>
2020-03-03 17:47:06 +0200

    feat(clevel_app): Add katex component

--- clevel_app/src/main.js
+++ clevel_app/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VuetifyDialog from 'vuetify-dialog';
+import VueKatex from 'vue-katex';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,7 @@ import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify-dialog/dist/vuetify-dialog.css';
+import 'katex/dist/katex.min.css';
 
 Vue.config.productionTip = false;
 
@@ -15,6 +17,8 @@ Vue.use(VuetifyDialog, {
     vuetify,
   },
 });
+
+Vue.use(VueKatex);
 
 new Vue({
   router,
