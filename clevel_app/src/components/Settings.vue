60dd3463c3ac21282965bb235e5b387694b677cc
Reidond <reidond@gmail.com>
2020-03-04 21:21:39 +0200

    feat(clevel_app): Translate Settings

--- clevel_app/src/components/Settings.vue
+++ clevel_app/src/components/Settings.vue
@@ -2,12 +2,14 @@
   <v-dialog v-model="dialog" max-width="500px">
     <template v-slot:activator="{ on }">
       <v-btn-toggle>
-        <v-btn color="accent" dense outlined v-on="on">Settings</v-btn>
+        <v-btn color="accent" dense outlined v-on="on">{{
+          $t('settings.btn')
+        }}</v-btn>
       </v-btn-toggle>
     </template>
     <v-card>
       <v-card-title>
-        <span class="headline">Settings</span>
+        <span class="headline">{{ $t('settings.card.title') }}</span>
       </v-card-title>
 
       <v-card-text>
@@ -37,8 +39,12 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-        <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+        <v-btn color="blue darken-1" text @click="close">{{
+          $t('settings.action-cancel')
+        }}</v-btn>
+        <v-btn color="blue darken-1" text @click="save">{{
+          $t('settings.action-save')
+        }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
